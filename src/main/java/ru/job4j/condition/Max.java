7e746f6c8d1c790ee79ada17407ec5e5b17c2bf7
Artur <aaa0981@mail.ru>
2022-07-01 11:39:21 +0300

    Тернарное сравнение [#189]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left >= right ? left : right;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        max(1,2);
+        max(2,1);
+        max(2,2);
+    }
+}
