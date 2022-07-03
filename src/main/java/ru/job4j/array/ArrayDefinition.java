fe626b5da0640275f1175e70d4f2f3e18b8ec4b9
Artur <aaa0981@mail.ru>
2022-07-03 09:33:17 +0300

    Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,5 +1,7 @@
 package ru.job4j.array;
 
+import java.util.MissingFormatArgumentException;
+
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
@@ -8,5 +10,13 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Джон Леннон";
+        names[1] = "Пол Маккартни";
+        names[2] = "Джордж Харрисон";
+        names[3] = "Ринго Старр";
+        for (String s : names) {
+            System.out.println(s);
+        }
     }
 }
